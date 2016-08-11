78627e50004dd0f8632cb99207ac8d09b2b13d0c
Matthew Phillips <matthew@matthewphillips.info>
2016-08-11 17:06:18 -0400

    3.0.0-pre.12

---
+++ dist/cjs/converters.js
@@ -0,0 +1,34 @@
+/*can-stache-bindings@3.0.0-pre.11#converters*/
+var stache = require('can-stache');
+var stringToAny = require('can-util/js/string-to-any/string-to-any');
+stache.registerConverter('boolean-to-inList', {
+    get: function (item, list) {
+        if (!list) {
+            return false;
+        } else {
+            return list.indexOf(item) !== -1;
+        }
+    },
+    set: function (newVal, item, list) {
+        if (!list) {
+            return;
+        }
+        if (!newVal) {
+            var idx = list.indexOf(item);
+            if (idx !== -1) {
+                list.splice(idx, 1);
+            }
+        } else {
+            list.push(item);
+        }
+    }
+});
+stache.registerConverter('string-to-any', {
+    get: function (compute) {
+        return '' + compute();
+    },
+    set: function (newVal, compute) {
+        var converted = stringToAny(newVal);
+        compute(converted);
+    }
+});
